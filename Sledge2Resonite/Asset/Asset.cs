80718d98a0c38f0333c77e1b1b80fcb4b4bfb54c
marsmaantje[pc] <marsmaantje@planet.nl>
2023-11-03 15:54:47 +0100

    (WIP) generics for asset importing

---
+++ Sledge2Resonite/Asset/Asset.cs
@@ -0,0 +1,31 @@
+﻿using FrooxEngine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sledge2Resonite
+{
+    internal abstract class Asset<T, U> where U : IAssetProvider
+    {
+        T asset;
+        public bool HasWorldElement { get; private set; }
+        public Slot WorldElementSlot { get; private set; }
+
+        public Asset(T asset)
+        {
+            this.asset = asset;
+            HasWorldElement = false;
+        }
+
+        public Task<Slot> CreateWorldElement(Slot target, bool createChild, bool generateVisual)
+        {
+            throw new NotImplementedException();
+        }
+
+        abstract protected void AttachAsset(Slot target);
+
+        abstract public void GeneratreVisual();
+    }
+}
---
+++ Sledge2Resonite/AssetImporter/AssetImporter.cs
@@ -0,0 +1,48 @@
+﻿using FrooxEngine;
+using Sledge.Formats.Texture.Vtf;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sledge2Resonite
+{
+    internal abstract class AssetImporter<T, U> where U : IAssetProvider
+    {
+        Dictionary<string, Asset<T, U>> assets { get; set; } = new();
+        public Asset<T, U> ImportAsset(string filePath)
+        {
+            //check if file exists
+            if (!File.Exists(filePath))
+            {
+                throw new FileNotFoundException("File not found", filePath);
+            }
+            string fileName = Path.GetFileName(filePath);
+            FileStream stream = new FileStream(filePath, FileMode.Open);
+            return ImportAsset(stream, fileName);
+        }
+
+        public Asset<T, U> ImportAsset(FileStream fileStream, string name)
+        {
+            //check if asset already exists
+            if (assets.ContainsKey(name))
+            {
+                return assets[name];
+            }
+
+            //check if file is valid
+            if (fileStream == null)
+            {
+                throw new NullReferenceException("fileStream is null");
+            }
+
+            Asset<T, U> asset = PreProcessAsset(fileStream);
+            assets.Add(name, asset);
+            return asset;
+        }
+
+        abstract protected Asset<T, U> PreProcessAsset(FileStream file);
+    }
+}
