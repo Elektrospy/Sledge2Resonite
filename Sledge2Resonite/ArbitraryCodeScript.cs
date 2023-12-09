7b888e2e3dd5154b468bf5d067f4f10e6431ee0c
marsmaantje[pc] <marsmaantje@planet.nl>
2023-12-09 22:22:06 +0100

    new files

---
+++ Sledge2Resonite/ArbitraryCodeScript.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FrooxEngine;
+using FrooxEngine.ProtoFlux;
+
+namespace Sledge2Resonite
+{
+    public class ArbitraryCodeScript
+    {
+        public ReferenceField<IWorldElement> target;
+
+        public string UpdateMeshColliderToItsRenderer(
+        {
+Slot targetSlot = (Slot)target.Reference.Target;
+int changeCount = 0;
+
+var renderers = targetSlot.GetComponentsInChildren<MeshRenderer>();
+
+for (int i = 0; i < renderers.Count; i++)
+{
+    try
+    {
+
+
+        var renderer = renderers[i];
+        var rendererSlot = renderer.Slot;
+        var collider = rendererSlot.GetComponent<MeshCollider>();
+        if (collider != null && collider.Mesh.Target != renderer.Mesh.Target)
+        {
+            collider.Mesh.TrySet(renderer.Mesh.Target);
+            changeCount++;
+        }
+    }
+    catch (Exception e) { }
+}
+
+
+return $"Changed {changeCount} meshColliders to their renderers, processed {renderers.Count} renderers in total";
+        }
+
+        public string ComponentTypeCount()
+        {
+            Slot targetslot = (Slot)target.Reference.Target;
+
+            var renderers = targetslot.GetComponentsInChildren<MeshRenderer>();
+
+            return $"found {renderers.Count} meshRenderers";
+        }
+    }
+}
---
+++ Sledge2Resonite/Asset/VTFAsset.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sledge.Formats.Texture.Vtf;
+using FrooxEngine;
+
+namespace Sledge2Resonite
+{
+    internal class VtfAsset : Asset<VtfFile, StaticTexture2D>
+    {
+        public VtfAsset(VtfFile asset) : base(asset)
+        {
+        }
+
+        public override void GeneratreVisual()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void AttachAsset(Slot target)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
